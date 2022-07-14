b9247ef3f85634ae4a3ab5877393181099b5e362
Michael Rodriguez <mmrodriguez593@hotmail.com>
2022-07-14 17:09:23 -0400

    final commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log(getAreaOfCircle(63));
+console.log(getCircumferenceOfCircle(12));
+console.log(getAreaOfsquare(45));
+console.log(getAreaOfTriangle(27, 19));
+
+
+
+
+
+
+
+
+function getAreaOfCircle (a){
+    return a / 2 * Math.PI;
+}
+
+function getCircumferenceOfCircle ( b) {
+    return b / 2 * Math.PI;
+}
+function getAreaOfsquare (s){
+    return (s-2) * 180;
+}
+function getAreaOfTriangle(b, h){
+    return b * h;
+}
+
+
